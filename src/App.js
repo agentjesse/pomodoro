da63ad09361affb7d9d69b5b046d1329ad34ded0
Jesse <31576321+agentjesse@users.noreply.github.com>
2017-10-25 00:28:36 -0400

    pre styling final

--- src/App.js
+++ src/App.js
@@ -9,42 +9,80 @@ class App extends Component {
     super(props);
     //set the state
     this.state = {
-      time: 13,//1500s is 25min
-      display: 0
+      time: 1500,//for production: set default 1500s as 25min
+      display: 0,
+      intervalId:undefined,//initially set from component mount, then custom button
+      customMins: 1500,
+      sound: new Audio('shipbell.wav')
     };
-    //run a method in this class component
-    this.startTimer();
   }
 
-  startTimer(){
-    //setInterval returns a value, let's call it interval. to stop the counter, call clearInterval(interval). 
-    let interval = setInterval( ()=>{
-      // update global time by 1, a second has passed from using setInterval
-      this.setState({time: this.state.time - 1});
+  componentDidMount() {
+    //setInterval returns a value, save it to state part 1/2. to stop the counter call clearInterval(this.state.intervalId)
+    let intervalId = setInterval( ()=> { this.timeFunc() }, 1000);
+    //setInterval returns a value, save it to state part 2/2. don't console log this, for some reason it will show up right away inaccurately.
+    this.setState({intervalId: intervalId});
+  }
 
-      //get minutes and seconds from current time. they are strings
-      let minutes = ('0'+ Math.floor( this.state.time/60 )).slice(-2);
-      let seconds = ('0'+ this.state.time % 60).slice(-2);
+  timeFunc(){
+    // update global time by 1, a second has passed from using setInterval
+    this.setState({time: this.state.time - 1});
+    
+    //check if timer finished and stop it
+    if (this.state.time === 0){
+      //use iD here as the iD to stop the timer at end before setting id to state
+      clearInterval(this.state.intervalId);
 
-      //check if timer finished and stop it
-      if (minutes === '00' && seconds === '00'){
-        clearInterval(interval);
-      }
+      //play sound element constructed in state
+      this.state.sound.play();
+      // console.log(this.state.sound);
 
-      //updating the state property display 
-      this.setState({
-        display: minutes + ':' + seconds
-        // display: this.state.time
-      });
+      //display the intervalId at zero.
+      // console.log('current running interval id: ',this.state.intervalId);
+    }
 
-    } ,1000);
+    //get minutes and seconds from current time. they are strings
+    let minutes = ('0'+ Math.floor( this.state.time/60 )).slice(-2);
+    let seconds = ('0'+ this.state.time % 60).slice(-2);
+
+    //update the state property display 
+    this.setState({
+      display: minutes + ':' + seconds
+      // display: this.state.time
+    });
   }
 
   render() {
     return (
       <div className="App">
+        {/* timer display */}
         <div id='timer'>{this.state.display}</div>
-        <button>restart pomodoro</button>
+
+        {/* custom time input */}
+        <input className='customTimeInput'
+          placeholder='enter custom minutes here'
+          onChange={ event => { this.setState({ customMins: Math.round(event.target.value * 60) }) } }
+        />
+        <br />
+        {/* restart button */}
+        <button
+          onClick={evt=>{
+            //stop setInterval
+            clearInterval(this.state.intervalId);
+            // set state time to customMins if not zero and is a valid number
+            if(this.state.customMins > 0 && typeof this.state.customMins === 'number'){
+              this.setState({time:this.state.customMins});
+            }//otherwise, reset to 25 mins
+            else{
+              this.setState({time:1500});
+            }
+            //to restart the timer you have to call set interval again and assign the new id to state to keep control of it.
+            let intervalId = setInterval( ()=> { this.timeFunc() }, 1000);
+            this.setState({intervalId: intervalId});
+            
+          }}>restart pomodoro
+        </button>
+
       </div>
     );
   }
